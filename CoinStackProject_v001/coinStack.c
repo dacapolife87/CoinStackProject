4331eaddf1c369199fbe1688c8e803c207a4360b
Hyungju Jang <dacapolife87@gmail.com>
2014-05-15 12:00:32 +0900

    Func make startMenu & gameSetting

--- CoinStackProject_v001/coinStack.c
+++ CoinStackProject_v001/coinStack.c
@@ -12,7 +12,7 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //<--------------------- Define Start--------------------->
 // Define SleepTime 
-#define SLEEPTIME 100
+#define BASESLEEPTIME 100
 #define DROPTIME 50
 #define ENDINGTIME 2000
 // Define ColorSetting
@@ -45,12 +45,12 @@ void setWindowSize();
 // main menu
 void startMenu();
 void startImage();
-void selectMenu();
+int selectMenu();
 // game start
 void startGame();
 // game default setting
-void gameLevelSelect();
-void gameSetting();
+int gameLevelSelect();
+void gameSetting(int);
 void gameBackground();
 void gameInfo();
 // game Func
@@ -66,13 +66,21 @@ void gameClear();
 void gotoxy(int,int);
 //<-------------- Declare Function ProtoType End-------------->
 
+//<-------------- Declare Global Variable Start-------------->
+double coinMovingSpeed = 0;
+//<-------------- Declare Global Variable End-------------->
 
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //<-------------- Program Function Start-------------->
 // program main
-void main(){}
+void main(void)
+{
+	startMenu();
+
+	return;
+}
 // program setting
 void setWindowSize()
 {
@@ -81,14 +89,119 @@ void setWindowSize()
 	return;
 }
 // main menu
-void startMenu(){}
-void startImage(){}
-void selectMenu(){}
+void startMenu()
+{
+	int menuNum=0;
+	//startImage();
+
+	printf("번호입력 : \n");
+	menuNum = selectMenu();
+	
+	// 영문자 입력시 무한루프......
+	while ( !((menuNum==1)|| (menuNum==2)) )
+	{
+		printf("재입력 : \n");
+		menuNum = selectMenu();
+	}
+	
+	printf("input selectNum : %d\n",menuNum);
+	////////////////////////////////////
+	if(menuNum == 1)
+	{
+		startGame();
+		return;
+	}
+	
+	return;
+}
+void startImage()
+{
+	ORIGINAL;
+	printf("   ###      ####     #####    #     #\n");
+	printf("  #   #    #    #      #      ##    #\n");
+	printf("  #        #    #      #      # ### #\n");
+	printf("  #   #    #    #      #      #    ##\n");
+	printf("   ###	    ####     #####    #     #\n");
+	printf("\n");
+	printf("                                 ####   #######    #       ###    #   #\n");
+	printf("                                 #         #      #  #    #   #   #  #\n");
+	printf("                                 ####      #     ######   #       ###\n");
+	printf("                                    #      #     #    #   #   #   #  #\n");
+	printf("                                 ####      #     #    #    ###    #   #\n");
+	printf("\n");
+	printf("                             1. 게임시작    \n");
+	printf("                             2. 게임종료   \n");
+	printf("                          원하시는 메뉴를 선택하세요.   \n");
+	
+	return;
+}
+int selectMenu()
+{
+	int selectMenuNum = '0';
+
+	scanf("%d", &selectMenuNum);
+	printf("selectNum %d\n",selectMenuNum);
+
+	return selectMenuNum;
+}
 // game start
-void startGame(){}
+void startGame()
+{
+	int gameLevel=0;
+	system("cls");
+	printf("startGameFunc gamelevel : %d\n",gameLevel);
+	gameLevel = gameLevelSelect();
+	printf("after gamelevel select : %d",gameLevel);
+	gameSetting(gameLevel);
+	
+}
 // game default setting
-void gameLevelSelect(){}
-void gameSetting(){}
+int gameLevelSelect()
+{
+	int selectLevel=1;
+	//system("cls");
+	printf("1. Level 1\n");
+	printf("2. Level 2\n");
+	printf("3. Level 3\n");
+	printf("4. Level 4\n");
+	printf("5. Level 5\n");
+	
+	scanf("%d",&selectLevel);
+
+	switch (selectLevel)
+	{
+	case 1:
+		return 1;
+	case 2:
+		return 2;
+	case 3:
+		return 3;
+	case 4:
+		return 4;
+	case 5:
+		return 5;
+	default:
+		selectLevel =1;
+		printf("범위내 숫자 아님 기본값 1 로 지정 : %d \n",selectLevel);
+		break;
+	}
+	
+	return selectLevel;
+}
+void gameSetting(int level)
+{
+	double movingSpeed=BASESLEEPTIME;
+	int i;
+
+	for(i=1;i<level;i++)
+	{
+		movingSpeed = (movingSpeed * 9)/10;
+	}
+
+	coinMovingSpeed = movingSpeed;
+	
+	return;
+}
 void gameBackground(){}
 void gameInfo(){}
 // game Func
