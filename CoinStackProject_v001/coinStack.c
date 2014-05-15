9a7e0aea14ab10fa8d25ae434a061304d366a985
Hyungju Jang <dacapolife87@gmail.com>
2014-05-15 10:14:25 +0900

    'baseproject'

---
+++ CoinStackProject_v001/coinStack.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <Windows.h>
+#include <conio.h>
+
+// program main
+void main(){}
+// program setting
+void setWindowSize(){}
+// main menu
+void startMenu(){}
+void startImage(){}
+void selectMenu(){}
+// game start
+void startGame(){}
+// game default setting
+void gameLevelSelect(){}
+void gameSetting(){}
+void gameBackground(){}
+void gameInfo(){}
+// game Func
+void coinMove(){}
+void coinDown(){}
+void coinStack(){}
+void coinBalanceCheck(){}
+void gameOver(){}
+void gameClear(){}
+// program func
+void gotoxy(){}
+
+
+
